5596bbe4f42ff122b2e44f17d0cc325acd8507a7
Ivan Krivov <vnk729@gmail.com>
2019-08-14 15:46:01 +0300

    update structure

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,5 +5,7 @@ import { version } from '../../package.json';
 
 program
   .version(version)
+  .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
