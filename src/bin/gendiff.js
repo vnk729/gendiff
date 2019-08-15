2357de9bf139d532633f1e8b34a8bc252e165538
Ivan Krivov <vnk729@gmail.com>
2019-08-15 19:01:42 +0300

    update

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,10 +2,12 @@
 
 import program from 'commander';
 import { version } from '../../package.json';
+import genDiff from '..';
 
 program
   .version(version)
-  .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
