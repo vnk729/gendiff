98e6d06a0f377426296cb4c5c49d3adb93543ab5
Ivan Krivov <vnk729@gmail.com>
2019-08-18 01:32:26 +0300

    add yaml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('gendiff', () => {
-  const before = `${__dirname}/__fixtures__/before.json`;
-  const after = `${__dirname}/__fixtures__/after.json`;
+describe('gendiff', () => {
   const expected = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf-8');
 
-  expect(genDiff(before, after)).toBe(expected);
+  test('json', () => {
+    const before = `${__dirname}/__fixtures__/before.json`;
+    const after = `${__dirname}/__fixtures__/after.json`;
+    expect(genDiff(before, after)).toBe(expected);
+  });
+
+  test('yml', () => {
+    const before = `${__dirname}/__fixtures__/before.yml`;
+    const after = `${__dirname}/__fixtures__/after.yml`;
+    expect(genDiff(before, after)).toBe(expected);
+  });
 });
