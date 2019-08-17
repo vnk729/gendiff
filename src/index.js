d9cd98d51b697835eabf5f177a7b347461183037
Ivan Krivov <vnk729@gmail.com>
2019-08-17 18:22:33 +0300

    update structure

--- src/index.js
+++ src/index.js
@@ -8,16 +8,18 @@ export default (pathToFile1, pathToFile2) => {
   const allKeys = [...Object.keys(file1), ...Object.keys(file2)];
   const uniqKeys = allKeys.reduce((acc, key) => (acc.includes(key) ? acc : [...acc, key]), []);
 
-  return uniqKeys.reduce((acc, key) => {
+  const result = uniqKeys.reduce((acc, key) => {
     if (file1[key] === file2[key]) {
-      return { ...acc, [`  ${key}`]: file2[key] };
+      return [...acc, `\n    ${key}: ${file2[key]}`];
     }
     if (_.has(file1, key) && !_.has(file2, key)) {
-      return { ...acc, [`- ${key}`]: file1[key] };
+      return [...acc, `\n  - ${key}: ${file1[key]}`];
     }
     if (!_.has(file1, key) && _.has(file2, key)) {
-      return { ...acc, [`+ ${key}`]: file2[key] };
+      return [...acc, `\n  + ${key}: ${file2[key]}`];
     }
-    return { ...acc, [`+ ${key}`]: file2[key], [`- ${key}`]: file1[key] };
-  }, {});
+    return [...acc, `\n  + ${key}: ${file2[key]}`, `\n  - ${key}: ${file1[key]}`];
+  }, []);
+
+  return `{${result}\n}`;
 };
